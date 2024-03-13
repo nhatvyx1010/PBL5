e74d7daea7564acf4ad70f9c2619b1b470ba06a0
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2024-03-13 22:46:56 +0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
+import {routes} from './routes'
+import DefaultComponent from './components/DefaultComponent/DefaultComponent';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+          <Routes>
+            {routes.map((route) => {
+              const Page = route.page
+              const Layout = route.isShowHeader ? DefaultComponent : Fragment
+              return (
+                  <Route key={route.path} path={route.path} element={
+                    <Layout>
+                      <Page />
+                    </Layout>
+                  } />
+                )
+            })}
+          </Routes>
+        </Router>
     </div>
   );
 }
