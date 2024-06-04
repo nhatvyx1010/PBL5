5c59a8067002dc583e14a6556a2066b8bf34e413
Nguyễn Phạm Nhật Vỹ <115072818+nhatvyx1010@users.noreply.github.com>
2024-06-04 09:12:33 +0700

    Add files via upload

--- user/src/App.js
+++ user/src/App.js
@@ -2,12 +2,28 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import { routes } from './routes'
 import DefaultComponent from './components/DefaultComponent/DefaultComponent';
+import DefaultComponentUser from './components/DefaultComponentUser/DefaultComponentUser';
 import { Fragment } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query'; 
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 const queryClient = new QueryClient(); 
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const user = useSelector(state => state.user);
+  // const [sessionActive, setSessionActive] = useState(false);
+  useEffect(() => {
+    console.log("gettoken: "+user.access_token);
+    if (user.access_token) {
+      setAuthenticated(true); // Nếu có token, đánh dấu là đã đăng nhập
+    }else{
+      setAuthenticated(false); // Nếu khong có token, đánh dấu là đã đăng nhập
+    }
+    // Kiểm tra các điều kiện khác của phiên làm việc ở đây nếu cần thiết
+  }, [user.access_token]);
+
   return (
     <div>
       <QueryClientProvider client={queryClient}> 
@@ -15,7 +31,7 @@ function App() {
           <Routes>
             {routes.map((route) => {
               const Page = route.page
-              const Layout = route.isShowHeader ? DefaultComponent : Fragment
+              const Layout = route.isShowHeader ?  authenticated ? DefaultComponentUser : DefaultComponent : Fragment
               return (
                 <Route key={route.path} path={route.path} element={
                   <Layout>
